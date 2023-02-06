52faf269220040b282a58c1a73783628bd684099
y94836sl <siqi.li-9@student.manchester.ac.uk>
2023-02-06 22:47:13 +0000

    implement sign in and login functionality, still need to fix the after login home page

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.urls import reverse_lazy
+from django.views import generic
 
 class HomePageView(TemplateView):
 	template_name = "home.html"
@@ -7,8 +9,9 @@ class HomePageView(TemplateView):
 class AboutPageView(TemplateView):
 	template_name = "about.html"
 	
-class LoginPageView(TemplateView):
-	template_name = "login.html"
+class LoginPageView(generic.CreateView):
+	success_url = reverse_lazy('home')
+	template_name = "registration/login.html"
 	
-class SignUpPageView(TemplateView):
-	template_name = "signup.html"
+#class SignUpPageView(TemplateView):
+#	template_name = "signup.html"
