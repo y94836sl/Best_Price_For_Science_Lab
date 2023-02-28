04f2e4e1a575d126c8b2ba14b76731378db577d8
y94836sl <siqi.li-9@student.manchester.ac.uk>
2023-02-28 00:15:21 +0000

    add inventory graph for dashboard

--- inventory/views.py
+++ inventory/views.py
@@ -2,17 +2,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+from django.contrib.auth.decorators import login_required
+
 
 # Create your views here.
+@login_required(login_url = 'login')
 def DashboardView(request):
 	return render(request, 'dashboard/dashboard.html')
 	
+@login_required(login_url = 'login')
 def staff(request):
 	return render(request, 'dashboard/staff.html')
 
+@login_required(login_url = 'login')
 def product(request):
 	return render(request, 'dashboard/product.html')
 
+@login_required(login_url = 'login')
 def order(request):
 	return render(request, 'dashboard/order.html')
 
