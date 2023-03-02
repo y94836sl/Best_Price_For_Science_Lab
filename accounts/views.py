e8b877ab1456c9df2fdff4cdc537ccb67d76f569
y94836sl <siqi.li-9@student.manchester.ac.uk>
2023-03-02 22:28:16 +0000

    Update navbar and 2 versions of profile card

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,18 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, TemplateView
 from .forms import CustomUserCreationForm
+import requests
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 class SignUpView(CreateView):
 	form_class = CustomUserCreationForm 
 	success_url = reverse_lazy('login') 
 	template_name = 'registration/signup.html'
-	
+
+@login_required(login_url = 'login')
+def ProfileView(request):
+	context = {
+		
+	}
+	return render(request, 'registration/profile.html',context)
--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
-
+from accounts import views as userViews
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('inventory/', include('inventory.urls')),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('profile/', userViews.ProfileView, name='profile'),
     path('', include('pages.urls')),
 ]
