60f3ff173d6c1dfcef000d49d19c44a0336d82b1
y94836sl <siqi.li-9@student.manchester.ac.uk>
2023-03-03 00:09:06 +0000

    add signal for profile

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.contrib.auth import get_user_model
-
 
 # Create your models here.
 class CustomUser(AbstractUser):
-	age = models.PositiveIntegerField(null=True, blank=True)
+    age = models.PositiveIntegerField(null=True, blank=True)
 
 class Profile(models.Model):
-	staff = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
-	phone = models.CharField(max_length=20, null=True)
-	address = models.CharField(max_length=200, null=True)
-	image = models.ImageField(default="defaultProfileImage.jpeg", upload_to="Profile_Images")
-	
-	def __str__(self):
-		return f'{self.staff.username}\'s Profile'
+    staff = models.OneToOneField(CustomUser, on_delete=models.CASCADE, null=True)
+    phone = models.CharField(max_length=20, null=True)
+    address = models.CharField(max_length=200, null=True)
+    image = models.ImageField(default="defaultProfileImage.jpeg", upload_to="Profile_Images")
+
+    def __str__(self):
+        return f'{self.staff.username}\'s Profile'
