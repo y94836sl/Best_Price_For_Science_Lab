533a24c49e5bfe1792645f3b8c6a33a14a4a9275
y94836sl <siqi.li-9@student.manchester.ac.uk>
2023-03-04 21:17:48 +0000

    *Add product form

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import DashboardView, staff, product, order
+from .views import DashboardView, staff, product, order, product_add
 
 urlpatterns = [
 	path('', DashboardView, name='dashboard'),
 	path('staff/', staff, name='staff'),
 	path('product/', product, name='product'),
+	path('product/add', product_add, name='product_add'),
 	path('order/', order, name='order'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,10 @@
 #<!--Source: Example HTML code from https://github.com/KenBroTech/Django-Inventory-Management-System-->
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import requests
 from django.contrib.auth.decorators import login_required
 from .models import Product
+from .forms import ProductForm
 
 # Create your views here.
 @login_required(login_url = 'login')
@@ -14,16 +15,30 @@ def DashboardView(request):
 def staff(request):
 	return render(request, 'dashboard/staff.html')
 
-@login_required(login_url = 'login')
 def product(request):
 	items = Product.objects.all() # Use ORM
 #	items = Product.objects.raw('SELECT * from product')
 	context = {
 		'items':items,
-		
 	}
 	
 	return render(request, 'dashboard/product.html', context)
+
+def product_add(request):
+	items = Product.objects.all() # Use ORM
+	if request.method == 'POST':
+		form = ProductForm(request.POST)
+		if form.is_valid():
+			form.save()
+			return redirect('product')
+	else:
+		form = ProductForm()
+		
+	context = {
+		'items':items,
+		'form': form,
+	}
+	return render(request, 'dashboard/product_add.html', context)
 
 @login_required(login_url = 'login')
 def order(request):
